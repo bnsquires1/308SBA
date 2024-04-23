9aa680e2b8df1bbbaa9fb3ceb55ee3d7564e77e7
Brittany Nicole <bnsquires1@gmail.com>
2024-04-23 00:20:34 -0400

    deletions

--- script.js
+++ script.js
@@ -137,37 +137,3 @@ LearnerSubmissions.forEach((submission) => {
                         if ((submission.submission.submitted_at) > (assignment.due_at)) {
                             grade -= assignment.points_possible * 0.1; // Late penalty
                         }
-    
-    //                     function getLearnerData(course, ag, submissions) {
-    //                         // here, we would process this data to achieve the desired result.
-    //                         const result = [
-    //                             {
-    //                                 id: 125,
-    //                                 avg: 0.985, // (47 + 150) / (50 + 150)
-    //                                 1: 0.94, // 47 / 50
-    //                                 2: 1.0 // 150 / 150
-    //                             },
-    //                             {
-    //                                 id: 132,
-    //                                 avg: 0.82, // (39 + 125) / (50 + 150)
-    //                                 1: 0.78, // 39 / 50
-    //                                 2: 0.833 // late: (140 - 15) / 150
-    //                             }
-    //                         ];
-    
-    //                         return result;
-    //                     }
-    
-    //                     const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-    
-    
-    //                     console.log(result);
-    
-    // //Using slice to copy a portion of an array (learner 125)
-    
-    
-    
-    
-    // // use.pop to remove submission 3
-    
-    
